525c0a8a5fcc2b9fc8d5af28daa1b72405903c64
Conner <connerccook@csu.fullerton.edu>
2021-01-01 11:32:32 -0800

    updated comments of solutions

--- two_sum/soln2.cpp
+++ two_sum/soln2.cpp
@@ -25,3 +25,12 @@ vector<int> twoNumberSum(vector<int> array, int targetSum) {
 }
 
 //This is a better solution because its O(n) time and O(n) space because we used hash tables
+
+//Basically this solution uses hash tables. We input all of the elements in the array into the hash table,
+// which is O(N) time.
+// To understand the solution we have to realize that we have a target sum and a given element and that 
+// there is only one solution in each array
+// Basically, we do another loop which is O(n) and we try to find the element that is the difference of the
+// target sum and the given element in the loop. We do this because the difference is the second element that 
+// sums to the target sum. And since there is only one solution, we can assume that that element is in the array
+// the find function is O(1) most of the time so we assume its O(1). We then push it into the array and return the answer
